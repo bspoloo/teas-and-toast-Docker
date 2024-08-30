a31363aba20a27eb940c1985b8d1dce246b58ce8
bspoloo <animetx098@gmail.com>
2024-08-30 18:01:43 -0400

    find meals or cocktails by name

--- src/app/atomic/organisms/SearchForm.tsx
+++ src/app/atomic/organisms/SearchForm.tsx
@@ -3,17 +3,18 @@ import Input from "../atoms/Input";
 
 type FormSearchProps = {
     handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    text: string
 }
 
-function SearchForm({ handleChange}:FormSearchProps) {
+function SearchForm({ handleChange, text}:FormSearchProps) {
 
     return (
         <div className="flex flex-col p-8 bg-white shadow-lg rounded-3xl gap-5   md:w-1/3 h-auto ">
-            <TextContent text="Insert the Ingredient: " className="block text-gray-700 text-sm font-bold mb-2" />
+            <TextContent text={text} className="block text-gray-700 text-sm font-bold mb-2" />
 
             <Input
                 onChange={handleChange}
-                placeholder="Insert the Ingredient"
+                placeholder={text}
                 classname="w-full px-4 py-2 border border-[#DBDFD0] rounded-3xl focus:outline-none focus:ring-1 focus:ring-[#AD343E] focus:border-transparent"
                 type="text" />
         </div>
--- src/app/atomic/organisms/SearchPresentation.tsx
+++ src/app/atomic/organisms/SearchPresentation.tsx
@@ -1,12 +1,17 @@
 import TextContent from "../atoms/TextContent";
 import SubTitle from "../atoms/SubTitle";
 
-function SearchPresentation() {
+type searchProps = {
+    title: string,
+    text: string
+}
+
+function SearchPresentation({title, text}: searchProps) {
     const className = 'text-5xl md:text-5xl text-center font-semibold';
     return (
         <div>
-            <SubTitle TextContent="Ingredients" className={className} />
-            <TextContent text="Add ingredients to find dishes or cocktails that match your search." className="text-center mt-8 text-sm text-[#495460]" />
+            <SubTitle TextContent={title} className={className} />
+            <TextContent text={text} className="text-center mt-8 text-sm text-[#495460]" />
         </div>
     );
 }
